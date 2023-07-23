befed84d32663d333de29b08e332e1f652bcd47b
samuel <samueldev8@gmail.com>
2023-07-22 21:58:39 -0300

    refact Read csv files block

---
+++ ETL/Pipes/ReadCsvPipe.cs
@@ -0,0 +1,37 @@
+﻿using CsvHelper;
+using DaemonTechChallenge.Helpers;
+using DaemonTechChallenge.Models;
+using System.Threading.Tasks.Dataflow;
+
+namespace DaemonTechChallenge.ETL.Pipes;
+
+public class ReadCsvPipe
+{
+    private readonly ICsvHelper _csvHelper;
+
+    public ReadCsvPipe(ICsvHelper csvHelper)
+    {
+        _csvHelper = csvHelper;
+    }
+
+    public TransformManyBlock<List<Stream>, T> CreateReadCsvBlock<T>()
+    {
+        var transformBlock = new TransformManyBlock<List<Stream>, T>(async listStream =>
+        {
+            var results = new List<T>();
+
+            Console.WriteLine("Reading CSV file.");
+
+            foreach (var stream in listStream)
+            {
+                using var csvStream = stream;
+                var items = await _csvHelper.ReadAsync<T>(csvStream);
+                results.AddRange(items);
+            }
+
+            return results;
+        });
+
+        return transformBlock;
+    }
+}
