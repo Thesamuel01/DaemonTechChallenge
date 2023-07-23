84ebe6745b47f760c903750abc942f3375fbed05
samuel <samueldev8@gmail.com>
2023-07-22 22:03:22 -0300

    implement database persist block

---
+++ ETL/Pipes/PersistDataPipe.cs
@@ -0,0 +1,49 @@
+﻿using DaemonTechChallenge.Helpers;
+using System.Data;
+using System.Threading.Tasks.Dataflow;
+
+namespace DaemonTechChallenge.ETL.Pipes;
+
+public class PersistDataPipe
+{
+    private readonly IDBConnection _connection;
+
+    public PersistDataPipe(IDBConnection connection)
+    {
+        _connection = connection;
+    }
+
+    public ActionBlock<DataTable> CreateStoreBlock(string tableName)
+    {
+        var actionBlock = new ActionBlock<DataTable>(async dataTable =>
+        {
+            var isOpen = _connection.Open();
+
+            Console.WriteLine($"Adding {dataTable.Rows.Count} rows in database");
+
+            try
+            {
+                if (isOpen)
+                {
+                    await _connection.SaveAsync(dataTable, tableName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                _ = await _connection.CloseAsync();
+            }
+        });
+
+        _ = actionBlock.Completion.ContinueWith(task =>
+        {
+            Console.WriteLine("Pipeline finished.");
+
+            _connection.Close();
+
+            Console.WriteLine("Pipeline finished.");
+        });
+
+        return actionBlock;
+    }
+}
