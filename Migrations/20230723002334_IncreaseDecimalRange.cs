34808f98c454f9803ec01cd47d37cecb31db38db
samuel <samueldev8@gmail.com>
2023-07-22 21:49:14 -0300

    add increse decimal range migration and change table engine migration

---
+++ Migrations/20230723002334_IncreaseDecimalRange.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DaemonTechChallenge.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseDecimalRange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VlTotal",
+                table: "DailyReport",
+                type: "decimal(20,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(14,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VlQuota",
+                table: "DailyReport",
+                type: "decimal(30,12)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(22,12)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VlPatrimLiq",
+                table: "DailyReport",
+                type: "decimal(20,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(14,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VlTotal",
+                table: "DailyReport",
+                type: "decimal(14,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(20,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VlQuota",
+                table: "DailyReport",
+                type: "decimal(22,12)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(30,12)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "VlPatrimLiq",
+                table: "DailyReport",
+                type: "decimal(14,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(20,2)");
+        }
+    }
+}
---
+++ Migrations/20230723002733_ChangeTableEngineToMyISAM.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DaemonTechChallenge.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTableEngineToMyISAM : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE DailyReport ENGINE = MyISAM;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE DailyReport ENGINE = InnoDB;");
+        }
+    }
+}
