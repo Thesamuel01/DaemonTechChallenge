e1d25df3d6ac27a3ae00b71db7d1b1909fe9b873
samuel <samueldev8@gmail.com>
2023-07-22 21:59:47 -0300

    implement datable format helper

---
+++ Helpers/DataTableFormatHelper.cs
@@ -0,0 +1,23 @@
+﻿using FastMember;
+using System.Data;
+
+namespace DaemonTechChallenge.Helpers;
+
+public class DataTableFormatHelper : IDataTableFormat
+{
+    private string[]? MembersOrder { get; set; }
+
+    public DataTableFormatHelper(string[]? membersOrder)
+    {
+        MembersOrder = membersOrder;
+    }
+
+    public DataTable Format<T>(IEnumerable<T> bulk)
+    {
+        var datatable = new DataTable();
+        using var reader = ObjectReader.Create(bulk, MembersOrder);
+        datatable.Load(reader);
+
+        return datatable;
+    }
+}
