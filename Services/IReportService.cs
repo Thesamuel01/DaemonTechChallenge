bb26e9b5e9765d8ff00ea349e77b916729b7a605
samuel <samueldev8@gmail.com>
2023-07-22 21:41:39 -0300

    add report service interface

---
+++ Services/IReportService.cs
@@ -0,0 +1,8 @@
+﻿using DaemonTechChallenge.Models;
+
+namespace DaemonTechChallenge.Services;
+
+public interface IReportService
+{
+    public Task<List<DailyReport>> GetReportsAsync(string CNPJ, DateTime? startDate, DateTime? endDate);
+}
