00906bf5de1db5b97ec1f5af8fed4a8fe72468ff
samuel <samueldev8@gmail.com>
2023-07-22 22:00:21 -0300

    refact Convert to datatable block

---
+++ ETL/Pipes/ConvertToDataTablePipe.cs
@@ -0,0 +1,28 @@
+﻿using DaemonTechChallenge.Helpers;
+using FastMember;
+using System.Data;
+using System.Threading.Tasks.Dataflow;
+
+namespace DaemonTechChallenge.ETL.Pipes;
+
+public class ConvertToDataTablePipe
+{
+    public readonly IDataTableFormat _dataTableFormat;
+
+    public ConvertToDataTablePipe(IDataTableFormat dataTableFormat)
+    {
+        _dataTableFormat = dataTableFormat;
+    }
+
+    public TransformBlock<IEnumerable<T>, DataTable> CreateConvertToDataTableBlock<T>()
+    {
+        var transformBLock = new TransformBlock<IEnumerable<T>, DataTable>(bulk =>
+        {
+            Console.WriteLine("Transforming data in DataTable.");
+
+            return _dataTableFormat.Format(bulk);
+        });
+
+        return transformBLock;
+    }
+}
