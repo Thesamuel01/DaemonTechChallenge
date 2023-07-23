c207e0bad851fef127bd6797052407f634669fca
samuel <samueldev8@gmail.com>
2023-07-22 21:55:42 -0300

    implement zip helper

---
+++ Helpers/ZipHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.IO.Compression;
+
+namespace DaemonTechChallenge.Helpers;
+
+public class ZipHelper : IZiperHelper
+{
+    public List<Stream> Read(Stream stream, string fileType)
+    {
+        var filesStreams = new List<Stream>();
+
+        using var zipArchive = new ZipArchive(stream, ZipArchiveMode.Read);
+        for (int i = 0; i < zipArchive.Entries.Count; i++)
+        {
+            var entry = zipArchive.Entries[i];
+            if (entry.Name.EndsWith(fileType))
+            {
+                var fileStream = new MemoryStream();
+                entry.Open().CopyTo(fileStream);
+                fileStream.Seek(0, SeekOrigin.Begin);
+                filesStreams.Add(fileStream);
+            }
+        }
+
+        return filesStreams;
+    }
+}
