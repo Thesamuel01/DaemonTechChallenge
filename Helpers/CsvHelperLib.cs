b1b5fab9f69c6620d18aeb0eadf81df5e2ffdf78
samuel <samueldev8@gmail.com>
2023-07-22 21:58:03 -0300

    implement csv helper

---
+++ Helpers/CsvHelperLib.cs
@@ -0,0 +1,36 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+
+namespace DaemonTechChallenge.Helpers;
+
+public class CsvHelperLib : ICsvHelper
+{
+    private readonly CsvConfiguration _configuration;
+    private ClassMap? ClassMap { get; set; }
+
+    public CsvHelperLib(CsvConfiguration configuration, ClassMap? classMap)
+    {
+        _configuration = configuration;
+        ClassMap = classMap;
+    }
+
+    public async Task<List<T>> ReadAsync<T>(Stream stream)
+    {
+        var results = new List<T>();
+
+        using var reader = new StreamReader(stream);
+        using var csv = new CsvReader(reader, _configuration);
+
+        if (ClassMap != null)
+        {
+            csv.Context.RegisterClassMap(ClassMap);
+        }
+
+        await foreach (var record in csv.GetRecordsAsync<T>())
+        {
+            results.Add(record);
+        }
+
+        return results;
+    }
+}
