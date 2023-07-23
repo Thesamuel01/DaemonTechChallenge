401513aacd81b2a8beb0782d93e9b814568b0901
samuel <samueldev8@gmail.com>
2023-07-22 22:00:56 -0300

    add db connection helper

---
+++ Helpers/IDBConnection.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace DaemonTechChallenge.Helpers;
+
+public interface IDBConnection
+{
+    public Task<bool> CloseAsync();
+    public Task<bool> OpenAsync();
+    public Task<bool> SaveAsync(DataTable bulk, string tablename);
+    public bool Close();
+    public bool Open();
+}
