d6abdf17c5a3b1881ff877c8e68b33cfb6e50124
samuel <samueldev8@gmail.com>
2023-07-22 21:42:20 -0300

    implement report service

---
+++ Services/ReportService.cs
@@ -0,0 +1,39 @@
+﻿using DaemonTechChallenge.Data;
+using DaemonTechChallenge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaemonTechChallenge.Services;
+
+public class ReportService : IReportService
+{
+    private readonly AppDbContext _context;
+
+    public ReportService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<DailyReport>> GetReportsAsync(string CNPJ, DateTime? startDate, DateTime? endDate)
+    {
+        var query = _context.DailyReport.AsQueryable();
+
+        if (string.IsNullOrEmpty(CNPJ))
+        {
+            throw new ArgumentException("CNPJ param is required.");
+        }
+
+        query = query.Where(r => r.CnpjFundo == CNPJ);
+
+        if (startDate.HasValue)
+        {
+            query = query.Where(r => r.DtComptc >= startDate.Value);
+        }
+
+        if (endDate.HasValue)
+        {
+            query = query.Where(r => r.DtComptc < endDate.Value);
+        }
+
+        return await query.ToListAsync();
+    }
+}
