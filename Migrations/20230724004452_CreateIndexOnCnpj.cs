4924a32ec155e2bf4948e8e3e6c451a377b5969d
samuel <samueldev8@gmail.com>
2023-07-23 22:14:00 -0300

    add index on cnpj to inprove api performance

---
+++ Migrations/20230724004452_CreateIndexOnCnpj.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DaemonTechChallenge.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIndexOnCnpj : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_DailyReport_Cnpj",
+                table: "DailyReport",
+                column: "CnpjFundo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_NomeDaTabela_Cnpj",
+                table: "DailyReport");
+        }
+    }
+}
